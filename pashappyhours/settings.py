d543d6751988d45ef76e088499ddc6fbf1ce5052
Ajeeshas41 <ajeeshas1992@gmail.com>
2021-12-10 00:19:02 +0530

    db changed + template update

--- pashappyhours/settings.py
+++ pashappyhours/settings.py
@@ -79,10 +79,20 @@ WSGI_APPLICATION = 'pashappyhours.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DBGame',
+        'USER': 'projectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
+
 }
 
 
